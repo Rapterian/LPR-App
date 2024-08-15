df29b69a1ad637e79b1d808f423b2e76b8188552
Ati247365 <141302483+Ati247365@users.noreply.github.com>
2024-08-15 15:56:13 +0200

    Update Program.cs
    menu Update

--- LPR App/Program.cs
+++ LPR App/Program.cs
@@ -1,30 +1,47 @@
+using Sytem;
 ﻿using LPR_App;
 
-double[,] A = {
-                { 1, 1 },
-                { 2, 1 },
-                { 1, 3 }
-              };
+namespace LPR_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Linear Programming Solver!");
 
-double[] b = { 0, 4, 5, 6 };
-double[] c = { 3, 2 };
+            // Display menu options
+            Console.WriteLine("1. Solve using Primal Simplex Algorithm");
+            Console.WriteLine("2. Solve using Branch & Bound Simplex Algorithm");
+            Console.WriteLine("3. Solve using Cutting Plane Algorithm");
+            Console.WriteLine("4. Perform Sensitivity Analysis");
+            Console.WriteLine("5. Exit");
 
-//double[,] solution = Algorithms.PrimalSimplex(A, b, c);
-//Algorithms.displayTableau(solution, c.Length, b.Length-1, "Optimal Solution:");
+            // Example data for testing the algorithms
+            double[,] A = {
+                            { 1, 1 },
+                            { 2, 1 },
+                            { 1, 3 }
+                          };
 
-//Primal Simplex
-TableauModel model = new TableauModel(A,b,c);
-model = Algorithms.PrimalSimplex(model);
-model.ToConsole("Optimal Solution:", false);
+            double[] b = { 0, 4, 5, 6 };
+            double[] c = { 3, 2 };
 
-//Cutting Plane Simplex
-Algorithms.CuttingPlane(A, b, c);
+            // Primal Simplex
+            TableauModel model = new TableauModel(A, b, c);
+            model = Algorithms.PrimalSimplex(model);
+            model.ToConsole("Optimal Solution:", false);
 
+            // Cutting Plane Simplex
+            Algorithms.CuttingPlane(A, b, c);
 
-double[] weight = { 12, 2, 1, 1, 4 };
-double[] value = { 4, 2, 2, 1, 10 };
-double weightLimit = 15;
+            // Branch & Bound Knapsack
+            double[] weight = { 12, 2, 1, 1, 4 };
+            double[] value = { 4, 2, 2, 1, 10 };
+            double weightLimit = 15;
 
-Algorithms.BranchBoundKnapsack(value,weight , weightLimit);
+            Algorithms.BranchBoundKnapsack(value, weight, weightLimit);
 
-Console.ReadLine();
+            Console.ReadLine();
+        }
+    }
+}
